1df07d09fef8f73dd2481176d697899dd5336a53
Jason Ronallo <jronallo@gmail.com>
2010-12-02 20:59:45 -0500

    use Rake namespace to avoid classic-namespace warning

--- spec/blacklight-sitemap_spec.rb
+++ spec/blacklight-sitemap_spec.rb
@@ -3,28 +3,28 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "BlacklightSitemap" do
   it "should create the task" do
     Rake::BlacklightSitemapTask.new
-    Task['blacklight:sitemap'].should be_a_kind_of Rake::Task
+    Rake::Task['blacklight:sitemap'].should be_a_kind_of Rake::Task
   end
-  
+
   it "should be able to have the url attribute" do
     task = Rake::BlacklightSitemapTask.new do |sm|
       sm.url = 'http://example.com'
     end
     task.url.should eq('http://example.com')
-  end  
-  
+  end
+
   it 'should set the ceiling to 50,000 as a default' do
     task = Rake::BlacklightSitemapTask.new
     task.ceiling.should == 50000
   end
-  
+
   it 'should allow the ceiling to be set to a different value' do
     task = Rake::BlacklightSitemapTask.new do |sm|
       sm.ceiling = 50
     end
     task.ceiling.should == 50
   end
-  
+
   it 'should store a value for whether to gzip or not' do
     task = Rake::BlacklightSitemapTask.new do |sm|
       sm.gzip = true
@@ -32,3 +32,4 @@ describe "BlacklightSitemap" do
     task.gzip.should be_true
   end
 end
+
